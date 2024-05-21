eecf60919d02dc2d91a3451845ddb283d62bf0fd
PKRUNAL867 <prajapatipinki1112@gmail.com>
2024-05-21 13:04:43 +0530

    Add files via upload

---
+++ Studentdetails.java
@@ -0,0 +1,13 @@
+
+public class Studentdetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number,age;
+		String name1;
+		myclass m1=new myclass();
+		m1.inputdata(2,22,"pinki");
+		m1.getdata();
+	}
+
+}
---
+++ myclass.java
@@ -0,0 +1,18 @@
+
+public class myclass {
+	int roll,age;
+	String name;
+	
+	void inputdata(int number,int age1,String name1)
+	{
+		roll=number;
+		age=age1;
+		name=name1;
+	}
+	void getdata()
+	{
+		System.out.println("name is"+name);
+		System.out.println("age is"+age);
+		System.out.println("roll no"+roll);
+	}
+}
